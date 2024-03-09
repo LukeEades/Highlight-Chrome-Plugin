df8ad1374bfa4834f7a8ff3ff46668ff540c3e33
Luke Eades <lukeeadeswork@gmail.com>
2024-03-09 14:03:49 -0800

    got a working model of highlighting across nodes

--- scripts/script.js
+++ scripts/script.js
@@ -12,21 +12,7 @@ document.addEventListener('keydown', ()=>{
     let canHighlight = false; 
     console.log(parent.childNodes); 
     if(parent.childNodes.length == 0){
-        let node = parent; 
-        let newNode = document.createElement("highlight"); 
-        newNode.style.backgroundColor = "yellow"; 
-
-        let text = parent.textContent; 
-        let before = document.createTextNode(text.slice(0, startOffset)); 
-        let middle = document.createTextNode(text.slice(startOffset, endOffset)); 
-        let after = document.createTextNode(text.slice(endOffset, text.length -1));
-        newNode.innerHTML = middle.textContent; 
-        node.parentNode.insertBefore(before, node); 
-        node.parentNode.insertBefore(newNode, node); 
-        node.parentNode.insertBefore(after, node); 
-        node.parentNode.removeChild(node); 
-        // console.log("before: ", before, "; highlight: ", highlight, "; after: ", after); 
-        // parent.textContent = `${before}<highlight style="background-color: yellow">${highlight}</highlight>${after}`;  
+        surroundNode(parent, startOffset, endOffset)
         return; 
     }
 
@@ -34,57 +20,41 @@ document.addEventListener('keydown', ()=>{
     for(let i = 0; i < nodes.length; i++){
         if(nodes[i].nodeName != "#text"){
             let textNodes = getTextNodes(nodes[i]); 
-            for(let j = 0; j < textNodes.length; j++){
-                if(checkNode(textNodes[j])){
+            for(let j =0; j< textNodes.length; j++){
+                if(textNodes[j] == end){
+                    canHighlight = false; 
+                    surroundNode(textNodes[j], 0, endOffset); 
+                    j += 2; 
                     return; 
-                } 
+                }
+                else if(canHighlight){
+                    surroundNode(textNodes[j], 0, textNodes[j].length); 
+                    j+= 2; 
+                }
+                else if(textNodes[j] == start){
+                    canHighlight = true; 
+                    surroundNode(textNodes[j], startOffset, textNodes[j].length); 
+                    j+= 2; 
+                }
             }
         }
         else{
-            console.log("end node: ", end); 
             if(nodes[i] == end){
-                console.log("end: ", nodes[i]); 
                 canHighlight = false; 
-                let text = end; 
-                let afterOffset = text.splitText(range.endOffset);
-                let replaceText = `<highlight style= "background-color: yellow;">${text.textContent}</highlight>${afterOffset.textContent}`
-                text.parentNode.innerHTML = text.parentNode.innerHTML.replace(text.textContent, replaceText); 
-                break;  
-            }else if(canHighlight){
-                console.log("middle f: ", nodes[i])
-                // surroundNode(nodes[i]); 
+                surroundNode(nodes[i], 0, endOffset); 
+                i += 2; 
+                return; 
+            }
+            else if(canHighlight){
+                surroundNode(nodes[i], 0, nodes[i].length); 
+                i+= 2; 
             }
             else if(nodes[i] == start){
-                console.log("start: ", nodes[i]); 
                 canHighlight = true; 
-                let text = start; 
-                let afterOffset = text.splitText(range.startOffset); 
-                text.parentNode.innerHTML = text.parentNode.innerHTML.replace(text.textContent, `<highlight style= "background-color: yellow;">${afterOffset.textContent}</highlight>`); 
-                continue;  
-            }
-        }
-        
-    }
-    function checkNode(node){
-        if(node == end){
-            console.log("end: ", node); 
-            canHighlight = false; 
-            let text = end; 
-            let afterOffset = text.splitText(range.endOffset);
-            let replaceText = `<highlight style= "background-color: yellow;">${text.textContent}</highlight>${afterOffset.textContent}`
-            text.parentNode.innerHTML = text.parentNode.innerHTML.replace(text.textContent, replaceText); 
-            return true; 
-        }else if(canHighlight){
-            console.log("middle: ", node); 
-            // surroundNode(node); 
-        }
-        else if(node == start){
-            console.log("start: ", node); 
-            canHighlight = true; 
-            let text = start; 
-            let afterOffset = text.splitText(range.startOffset); 
-            text.parentNode.innerHTML.replace(text.textContent, `<highlight style= "background-color: yellow;">${afterOffset.textContent}</highlight>`); 
-        } 
+                surroundNode(nodes[i], startOffset, nodes[i].length); 
+                i+= 2; 
+            } 
+        }        
     }
 
 })    
@@ -106,12 +76,18 @@ function getTextNodes(node, textNodes = []){
     return textNodes; 
 }
 
-function surroundNode(node){
+function surroundNode(node, start, end){
     let parent = node.parentNode; 
-    let newNode = document.createElement("highlight");
+    let newNode = document.createElement("highlight"); 
     newNode.style.backgroundColor = "yellow"; 
+    let text = node.textContent; 
+    let before = document.createTextNode(text.slice(0, start)); 
+    let middle = document.createTextNode(text.slice(start, end)); 
+    let after = document.createTextNode(text.slice(end, text.length));
+    newNode.innerHTML = middle.textContent; 
+    parent.insertBefore(before, node); 
     parent.insertBefore(newNode, node); 
-    parent.removeChild(node); 
-    newNode.appendChild(node); 
-    return parent; 
+    parent.insertBefore(after, node); 
+    parent.removeChild(node);  
+    return; 
 }
