a1b0a62b7592e1ae80979b99da47df3eee34865a
Luke Eades <lukeeadeswork@gmail.com>
2024-03-09 13:18:42 -0800

    got highlighting to actually work with single strings

--- scripts/script.js
+++ scripts/script.js
@@ -8,74 +8,86 @@ document.addEventListener('keydown', ()=>{
     let start = range.startContainer; 
     let end = range.endContainer; 
     let endOffset = range.endOffset; 
+    let startOffset = range.startOffset; 
     let canHighlight = false; 
     console.log(parent.childNodes); 
     if(parent.childNodes.length == 0){
-        let textHighlight = parent.textContent.slice(range.startOffset, range.endOffset); 
-        // Try checking for right location later; currently is taking first match
-        parent.parentNode.innerHTML = parent.parentNode.innerHTML.replace(textHighlight, `<highlight style="background-color: yellow">${textHighlight}</highlight>`); 
+        let node = parent; 
+        let newNode = document.createElement("highlight"); 
+        newNode.style.backgroundColor = "yellow"; 
+
+        let text = parent.textContent; 
+        let before = document.createTextNode(text.slice(0, startOffset)); 
+        let middle = document.createTextNode(text.slice(startOffset, endOffset)); 
+        let after = document.createTextNode(text.slice(endOffset, text.length -1));
+        newNode.innerHTML = middle.textContent; 
+        node.parentNode.insertBefore(before, node); 
+        node.parentNode.insertBefore(newNode, node); 
+        node.parentNode.insertBefore(after, node); 
+        node.parentNode.removeChild(node); 
+        // console.log("before: ", before, "; highlight: ", highlight, "; after: ", after); 
+        // parent.textContent = `${before}<highlight style="background-color: yellow">${highlight}</highlight>${after}`;  
         return; 
     }
 
     let nodes = parent.childNodes; 
-    for(let i = 0; i < parent.childNodes.length; i++){
-        if(nodes[i].contains(end)){
-            if(end.nodeType != "#text"){
-                break; 
-            }
-            canHighlight = false; 
+    for(let i = 0; i < nodes.length; i++){
+        if(nodes[i].nodeName != "#text"){
             let textNodes = getTextNodes(nodes[i]); 
-            for(let i = 0; i < textNodes.length; i++){
-                // ERROR: Messing with child node in here somewhere
-                if(textNodes[i] == end){
-                    let text = end; 
-                    let afterOffset = text.splitText(range.endOffset);
-                    // text.parentNode.innerHTML = `<highlight style= "background-color: yellow;">${text.textContent}</highlight>${afterOffset.textContent}`
-                    text.parentNode.innerHTML = text.parentNode.innerHTML.replace(text.textContent, `<highlight style= "background-color: yellow;">${text.textContent}</highlight>`); 
-                    break; 
-                }
-                surroundNode(textNodes[i]); 
-                
-            }
-            break; 
-        }
-        else if(canHighlight){
-            // console.log("hell")
-            if(nodes[i].nodeName == "#text"){
-                surroundNode(nodes[i]); 
-                continue; 
-            }
-            let textNodes = getTextNodes(nodes[i]); 
-            for(let i = 0; i < textNodes.length; i++){
-                surroundNode(textNodes[i]); 
+            for(let j = 0; j < textNodes.length; j++){
+                if(checkNode(textNodes[j])){
+                    return; 
+                } 
             }
         }
-        else if(nodes[i].contains(start)){
-            canHighlight = true; 
-            if(start.nodeName != "#text"){
-                continue; 
+        else{
+            console.log("end node: ", end); 
+            if(nodes[i] == end){
+                console.log("end: ", nodes[i]); 
+                canHighlight = false; 
+                let text = end; 
+                let afterOffset = text.splitText(range.endOffset);
+                let replaceText = `<highlight style= "background-color: yellow;">${text.textContent}</highlight>${afterOffset.textContent}`
+                text.parentNode.innerHTML = text.parentNode.innerHTML.replace(text.textContent, replaceText); 
+                break;  
+            }else if(canHighlight){
+                console.log("middle f: ", nodes[i])
+                // surroundNode(nodes[i]); 
             }
-            if(nodes[i] == start){
-                console.log(nodes[i].parentNode)
-                // console.log("hello")
+            else if(nodes[i] == start){
+                console.log("start: ", nodes[i]); 
+                canHighlight = true; 
                 let text = start; 
                 let afterOffset = text.splitText(range.startOffset); 
-                text.parentNode.innerHTML.replace(text.textContent, `<highlight style= "background-color: yellow;">${afterOffset.textContent}</highlight>`); 
-            }
-            let textNodes = getTextNodes(nodes[i]); 
-            for(let i = 0; i < textNodes.length; i++){
-                if(textNodes[i] == start){
-                    let text = start; 
-                    let afterOffset = text.splitText(range.startOffset); 
-                    text.parentNode.innerHTML.replace(text.textContent, `<highlight style= "background-color: yellow;">${afterOffset.textContent}</highlight>`); 
-                    continue; 
-                }
-                surroundNode(textNodes[i]); 
+                text.parentNode.innerHTML = text.parentNode.innerHTML.replace(text.textContent, `<highlight style= "background-color: yellow;">${afterOffset.textContent}</highlight>`); 
+                continue;  
             }
         }
         
-    // selected.collapseToEnd(); 
-}})
+    }
+    function checkNode(node){
+        if(node == end){
+            console.log("end: ", node); 
+            canHighlight = false; 
+            let text = end; 
+            let afterOffset = text.splitText(range.endOffset);
+            let replaceText = `<highlight style= "background-color: yellow;">${text.textContent}</highlight>${afterOffset.textContent}`
+            text.parentNode.innerHTML = text.parentNode.innerHTML.replace(text.textContent, replaceText); 
+            return true; 
+        }else if(canHighlight){
+            console.log("middle: ", node); 
+            // surroundNode(node); 
+        }
+        else if(node == start){
+            console.log("start: ", node); 
+            canHighlight = true; 
+            let text = start; 
+            let afterOffset = text.splitText(range.startOffset); 
+            text.parentNode.innerHTML.replace(text.textContent, `<highlight style= "background-color: yellow;">${afterOffset.textContent}</highlight>`); 
+        } 
+    }
+
+})    
 
 // returns array of all descending text nodes from a node
 function getTextNodes(node, textNodes = []){
@@ -98,11 +110,8 @@ function surroundNode(node){
     let parent = node.parentNode; 
     let newNode = document.createElement("highlight");
     newNode.style.backgroundColor = "yellow"; 
-    // newNode.innerHTML = node.textContent; 
-    // parent.insertBefore(newNode, node); 
-    // parent.removeChild(node); 
+    parent.insertBefore(newNode, node); 
     parent.removeChild(node); 
-    parent.appendChild(newNode); 
     newNode.appendChild(node); 
-    return node; 
+    return parent; 
 }
